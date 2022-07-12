8ede00a48dc7a0c976f21e79ada0495974b99ea1
AyushMainali123 <ayushmainali11@gmail.com>
2022-07-12 12:46:13 +0545

    build(package): added nprogress

--- theme/styles.ts
+++ theme/styles.ts
@@ -1,14 +1,14 @@
 const theme = {
   styles: {
-        global: {
-          '*, *::before, *::after': {
-                boxSizing: 'borderBox',
-                padding: 0,
-                margin: 0
-          },
-          "body": {
-                backgroundColor: "baseBackground"
-            }
+            global: {                  
+              '*, *::before, *::after': {
+                        boxSizing: 'borderBox',
+                        padding: 0,
+                        margin: 0
+              },
+              "body": {
+                    backgroundColor: "baseBackground"
+              }
         }
   },
 }
