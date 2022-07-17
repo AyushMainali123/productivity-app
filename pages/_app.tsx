c3a26033624316c8835d22f17cd83e214def881e
AyushMainali123 <ayushmainali11@gmail.com>
2022-07-17 17:19:51 +0545

    feat: wrapper react-query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,9 @@ import { Provider } from 'react-redux'
 import NextNProgress from 'nextjs-progressbar'
 import { SessionProvider } from "next-auth/react"
 import AuthGuard from 'components/AuthGuard'
-
+import { QueryClient, QueryClientProvider, QueryKey } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import axios from 'axios'
 
 // Custom Interface for including requireAuth props passed by each page.
 interface MyAppProps extends AppProps {
@@ -17,24 +19,41 @@ interface MyAppProps extends AppProps {
 }
 
 
+const defaultQueryFn = async ({ queryKey }: { queryKey: QueryKey }) => {
+  const { data } = await axios.get(`${queryKey[0]}`);
+  return data;
+};
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: defaultQueryFn
+    },
+  },
+});
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }: MyAppProps) {
   return (
     <SessionProvider session={session}>
-      <Provider store={store}>
-        <ChakraProvider theme={theme}>
-          <NextNProgress
-            color="#BEE3F8"
-            height={5}
-          />
-          {Component.requireAuth ? (
-            <AuthGuard>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <ChakraProvider theme={theme}>
+            <NextNProgress
+              color="#BEE3F8"
+              height={5}
+            />
+            {Component.requireAuth ? (
+              <AuthGuard>
+                <Component {...pageProps} />
+              </AuthGuard>
+            ) : (
               <Component {...pageProps} />
-            </AuthGuard>
-          ) : (
-            <Component {...pageProps} />
-          )}
-        </ChakraProvider>
-      </Provider>
+            )}
+          </ChakraProvider>
+        </Provider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </SessionProvider>
   )
 }
