a27f7085659ea220fd173a926bc353636597ccd6
AyushMainali123 <ayushmainali11@gmail.com>
2022-07-18 16:43:01 +0545

    feat: integrated '/session/update' to make setting page dynamic

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,8 +1,72 @@
-import { Box, HStack, Select, StackItem } from "@chakra-ui/react"
+import { Box, HStack, Select, Skeleton, StackItem, useToast } from "@chakra-ui/react"
 import { Icon } from "@iconify/react"
+import axios from "axios"
 import AuthLayout from "components/Layouts/AuthLayout"
+import { useMutation, useQuery, useQueryClient } from "react-query"
+
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                              Utils Starts Here                             */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * * Update pomodoro length based on user changed value
+ */
+const updatePomodoroFn = async ({ key, value }: { key: "pomodoroLength" | "shortBreakLength" | "longBreakLength", value: number }) => {
+
+    const response = await axios.put(`/api/user/sessions/update`, {
+        [key]: value
+    })
+
+    return response;
+}
+
+/* ----------------------------- Utils Ends Here ---------------------------- */
 
 const Settings = () => {
+
+
+    const queryClient = useQueryClient()
+    const { mutateAsync, isLoading: isMutatePomodoroLoading } = useMutation(updatePomodoroFn, {
+        // When mutate is called:
+        onMutate: async newUserDetails => {
+            // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
+            await queryClient.cancelQueries(['/api/user'])
+
+            // Snapshot the previous value
+            const previousUser = queryClient.getQueryData<UserDetailsApiResponse>(['/api/user'])
+
+            // Optimistically update to the new value
+            queryClient.setQueryData<UserDetailsApiResponse>(['/api/user'], old => old && ({...old, [newUserDetails.key]: newUserDetails.value}))
+
+            // Return a context object with the snapshotted value
+            return { previousUser }
+        },
+        onError: (err, newnewUserDetail, context) => {
+            queryClient.setQueryData(['/api/user'], context?.previousUser)
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries(["/api/user"])
+        }
+    })
+    const { data: currentUser, isLoading: isCurrentUserLoading, isError: isCurrentUserError } = useQuery<UserDetailsApiResponse>(["/api/user"])
+
+
+    const toast = useToast({
+        position: "top-right",
+        variant: "solid"
+    })
+
+    if (isCurrentUserError) {
+        toast({
+            title: "Error loading current pomodoro details...",
+            isClosable: true,
+            duration: 6000
+        })
+    }
+
     return (
         <AuthLayout>
             <Box as={"section"} maxW={"8xl"} m={"auto"} px={{ base: 0, md: "30px", '2xl': "0px" }}>
@@ -23,27 +87,63 @@ const Settings = () => {
                             Pomodoro Length
                         </StackItem>
                         <StackItem m={0}>
-                            <Select defaultValue={1} >
-                                <option value={1}>1 minutes</option>
-                                <option value={2}>2 minutes</option>
-                                <option value={3}>3 minutes</option>
-                            </Select>
+                            <Skeleton isLoaded={!isCurrentUserLoading}>
+                                <Select
+                                    value={currentUser?.pomodoroLength}
+                                    onChange={(e) => mutateAsync({ key: "pomodoroLength", value: Number(e.target.value) })}
+                                    disabled={isMutatePomodoroLoading}
+                                >
+                                    {
+                                        Array.from({ length: 59 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num} minutes</option>)
+                                    }
+                                </Select>
+                            </Skeleton>
                         </StackItem>
                     </HStack>
                 </Box>
 
-                {/* Break Length Section */}
+
+                {/*Short Break Length Section */}
                 <Box my={7}>
                     <HStack flexWrap={"wrap"} gap={1}>
                         <StackItem as={"h5"} width={"150px"}>
-                            Break Length
+                            Short Break Length
                         </StackItem>
                         <StackItem m={0}>
-                            <Select defaultValue={1}>
-                                <option value={1}>1 minutes</option>
-                                <option value={2}>2 minutes</option>
-                                <option value={3}>3 minutes</option>
-                            </Select>
+                            <Skeleton isLoaded={!isCurrentUserLoading}>
+                                <Select
+                                    value={currentUser?.shortBreakLength}
+                                    onChange={(e) => mutateAsync({ key: "shortBreakLength", value: Number(e.target.value) })}
+                                    disabled={isMutatePomodoroLoading}
+                                >
+                                    {
+                                        Array.from({ length: 59 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num} minutes</option>)
+                                    }
+                                </Select>
+                            </Skeleton>
+                        </StackItem>
+                    </HStack>
+                </Box>
+
+
+                {/*Long Break Length Section */}
+                <Box my={7}>
+                    <HStack flexWrap={"wrap"} gap={1}>
+                        <StackItem as={"h5"} width={"150px"}>
+                            Long Break Length
+                        </StackItem>
+                        <StackItem m={0}>
+                            <Skeleton isLoaded={!isCurrentUserLoading}>
+                                <Select
+                                    value={currentUser?.longBreakLength}
+                                    onChange={(e) => mutateAsync({ key: "longBreakLength", value: Number(e.target.value) })}
+                                    disabled={isMutatePomodoroLoading}
+                                >
+                                    {
+                                        Array.from({ length: 59 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num} minutes</option>)
+                                    }
+                                </Select>
+                            </Skeleton>
                         </StackItem>
                     </HStack>
                 </Box>
