947bd427e3dcc1be4aa5f730af17c132cf4df5c7
AyushMainali123 <ayushmainali11@gmail.com>
2022-07-23 22:29:42 +0545

    refractor: replaced the utils to global utils folder

--- utils/getTotalCompletedPercentage.ts
+++ utils/getTotalCompletedPercentage.ts
@@ -1,9 +1,12 @@
-
 export const getTotalCompletedPercentage = (workSessions: WorkSessionApiResponse[]) => {
     
     // This value is in multiple of 100. i.e. 61 means 61percentage of completed task.
     const completedSessions = workSessions.reduce((prev: number, acc: WorkSessionApiResponse) => {
-            return prev + acc.completedPercentage
+
+        if (acc.sessionType !== "WORK_SESSION") return prev;
+
+        return prev + acc.completedPercentage
+        
     }, 0)
 
 
